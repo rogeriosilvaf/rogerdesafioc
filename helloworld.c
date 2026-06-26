ba01953db81fe4ef0b3f627122be7fefef6994c3
AndreyMikeCoder <andreyomike@gmail.com>
2026-06-26 15:35:57 -0300

    hello rogercoder

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+    int main(){
+        //entra altura e peso (variaveis)
+        float altura;
+        float peso;
+// entrada de dados
+ printf("insira altura\n");
+ scanf("%f", &altura);
+
+
+
+
+        return 0;
+    }
