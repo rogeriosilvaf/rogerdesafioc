b594066a56e6b246750162b88781419da9fa8c6b
rogeriosilvaf <rogeriopedro.aa@gmail.com>
2026-06-26 15:51:30 -0300

    passo pro proximo

--- helloworld.c
+++ helloworld.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
-    int main(){
-        //entra altura e peso (variaveis)
-        float altura;
-        float peso;
+
+int main(){
+    //entra altura e peso (variaveis)
+    float altura;
+    float peso;
+
 // entrada de dados
- printf("insira altura\n");
- scanf("%f", &altura);
+    printf("insira altura\n");
+    scanf("%f", &altura);
+    printf("insira peso\n");
+    scanf("%f", &peso);
 
+    float result = altura * altura / peso;
 
+    printf("%f", result);
 
-
-        return 0;
-    }
+    return 0;
+}
